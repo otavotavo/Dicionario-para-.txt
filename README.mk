3f8b125794aade8180a7bb255ecd1e4817f85eb3
otavotavo <octaviogodoy123@gmail.com>
2019-11-12 15:32:06 -0300

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,9 @@
+(PROJETO EM ANDAMENTO)
+Dicionário gerado a partir de um arquivo .txt que separa as palavras do texto do arquivo (usando lista encadeada para armazena-las)
+e com um contador de frequência das palavras sendo a saída do programa um arquivo com o dicionário gerado.Com um menu de opçoes 
+iniciais.
+
+Projeto direcionado exclusivamente para linux (.ELF).
+
+ESTADO ATUAL-
+Separação das palavras.
