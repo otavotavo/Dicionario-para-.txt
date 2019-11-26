60918245a7e0c43f1c2eb2bd23f0a0c18352264b
otavotavo <octaviogodoy123@gmail.com>
2019-11-26 17:35:09 -0300

    Update README.mk

--- README.mk
+++ README.mk
@@ -1,9 +1,7 @@
-(PROJETO EM ANDAMENTO)
 Dicionário gerado a partir de um arquivo .txt que separa as palavras do texto do arquivo (usando lista encadeada para armazena-las)
-e com um contador de frequência das palavras sendo a saída do programa um arquivo com o dicionário gerado.Com um menu de opçoes 
+e com um contador de frequência das palavras. Sendo a saída do programa um arquivo com o dicionário gerado. Com um menu de opçoes 
 iniciais.
 
-Projeto direcionado exclusivamente para linux (.ELF).
+Projeto direcionado exclusivamente para linux (.ELF 64bits).
 
-ESTADO ATUAL-
-Separação das palavras.
+Uso: "./dicionario arquivo-desejado"
